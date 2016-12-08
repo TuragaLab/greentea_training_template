b96a5045598ba590ea8c2cab16e37e0f427de3c2
William Grisaitis <grisaitisw@janelia.hhmi.org>
2016-12-08 10:45:13 -0500

    Config for run_1208_5 - like fibsem32, with 12/5/16 caffe, CUDA 7.5, libdnn

--- start.sh
+++ start.sh
@@ -6,13 +6,13 @@ export NAME=$(basename "$PWD")
 
 nvidia-docker rm $NAME
 
-NV_GPU=1 \
+NV_GPU=3 \
     nvidia-docker run -d \
     -u `id -u $USER` \
     -v $(pwd):/workspace \
     -v /groups/turaga/home:/groups/turaga/home \
     -v /nrs/turaga:/nrs/turaga:shared \
     --name $NAME \
-    turagalab/greentea:cudnn5-caffe_gt-2016.12.05-pygt-0.9.4b \
+    turagalab/greentea:libdnn-caffe_gt-2016.12.05-pygt-0.9.4b \
     python -u train.py
 
